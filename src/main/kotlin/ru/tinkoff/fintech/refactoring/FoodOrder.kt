b09988e4d3fe3ffe52b47e6217419ee13e8e3c54
Grunskii Alexey <robochat@mail.ru>
2022-03-07 02:45:06 +0300

    Misstake task

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/FoodOrder.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/FoodOrder.kt
@@ -2,7 +2,7 @@ package ru.tinkoff.fintech.refactoring
 
 import ru.tinkoff.fintech.refactoring.food.SimpleFood
 
-data class FoodOrder<T : SimpleFood>(
+data class FoodOrder<out T : SimpleFood>(
     val orderId: Int,
     val food: T
 )
--- src/main/kotlin/ru/tinkoff/fintech/refactoring/employee/Employee.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/employee/Employee.kt
@@ -3,7 +3,7 @@ package ru.tinkoff.fintech.refactoring.employee
 import ru.tinkoff.fintech.refactoring.FoodOrder
 import ru.tinkoff.fintech.refactoring.food.SimpleFood
 
-interface Employee<T : SimpleFood> {
+interface Employee<in T : SimpleFood> {
     fun canCook(order: FoodOrder<SimpleFood>): Boolean
 
     fun doWork(order: FoodOrder<T>)
